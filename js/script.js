5236cccf3511f72342297f92d5937c090e381c25
praantosamadder <praanto.samadder07@gmail.com>
2021-01-25 02:47:24 +0600

    Fix v.1.0 on Apple Safari for WHY EKE logo overflow

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Detect device type
+const getDeviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        return "tablet";
+    }
+    if (
+        /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+            ua
+        )
+    ) {
+        return "mobile";
+    }
+    return "desktop";
+};
+
+// If not mobile
+if (getDeviceType() !== "mobile") {
+    // TODO redirect to desktop site
+    console.log("Not running on a mobile device")
+    // window.location.replace("")
+}
