01b349c1c30c718156d21bd07f1e1d3522c77e32
praantosamadder <praanto.samadder07@gmail.com>
2021-01-25 02:34:38 +0600

    New Design + Added why_eke label

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Sticky header animation
-let front_card_height = document.getElementById("front_card").clientHeight - document.getElementById("top_header").clientHeight
-console.log(front_card_height)
-
-window.onscroll = () => {
-    if (window.pageYOffset >= front_card_height) {
-    }
-}
