390828aee180881c4ffb3a0dfdeb806b52909514
praantosamadder <praanto.samadder07@gmail.com>
2021-01-28 02:29:36 +0600

    Added menu close icon

--- js/nav_menu.js
+++ js/nav_menu.js
@@ -1,6 +1,7 @@
 let hamburgerButton     = document.getElementById("nav-hamburger-container")
 let everythingButNav    = document.getElementById("everything-but-nav")
 let navMenu             = document.getElementById("nav-menu")
+let hamburgerIcon       = document.getElementById("hamburger-icon")
 
 let isNavMenuVisible = false
 
@@ -20,12 +21,14 @@ function hideNavMenu() {
     navMenu.style.display = "none"
     everythingButNav.style.display = "inherit"
     isNavMenuVisible = false
+    hamburgerIcon.src = "./img/icons/menu_hamburger.svg"
 }
 
 function showNavMenu() {
     navMenu.style.display = "inherit"
     everythingButNav.style.display = "none"
     isNavMenuVisible = true
+    hamburgerIcon.src = "./img/icons/x.svg"
 }
 
 function onClick_NavMenuItem(linkId) {
