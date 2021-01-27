3223bb23822738c32a3c4cd9773d16bb1c79bbb1
praantosamadder <praanto.samadder07@gmail.com>
2021-01-27 13:39:38 +0600

    Prevented img downloads on headers

---
+++ js/device_config.js
@@ -0,0 +1,5 @@
+// IF display width is less than display height
+
+// IF device is not mobile, then redirect to ekeducation.org
+
+// IF current browser is Chrome on Android, then change stylesheets to Android compatible version
