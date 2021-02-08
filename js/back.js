209e85875d215b536cd1d0f148a0362e91958f47
praantosamadder <praanto.samadder07@gmail.com>
2021-02-08 22:58:06 +0600

    Added two columns

--- js/back.js
+++ js/back.js
@@ -1,5 +1,5 @@
 let backButton = document.getElementById("back-button")
 
 backButton.onclick = () => {
-    window.location.replace("https://praantosamadder.github.io/m.ekeducation.org/index.html")
+    window.location.replace("https://m.ekeducation.org/")
 }
