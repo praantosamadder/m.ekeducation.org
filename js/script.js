3c1cff64bb1b2769781c211609e66290a79dad96
praantosamadder <praanto.samadder07@gmail.com>
2021-02-09 00:25:06 +0600

    Added Chrome on Android compatibility

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-// alert("Added padding-right to cv_sl_subject_name + updated location text size")
-// alert("AppCodeName" + navigator.appCodeName)
+let md = new MobileDetect(navigator.userAgent)
 
+if (md.os() === "AndroidOS" && md.userAgent() === "Chrome") {
+    document.getElementById("top_header_css").href = "./css_v8/top_header.css"
+    document.getElementById("styles_css").href = "./css_v8/styles.css"
+    document.getElementById("wye_css").href = "./css_v8/wye.css"
+    document.getElementById("nav_menu_css").href = "./css_v8/nav_menu.css"
+    document.getElementById("courses_offered_css").href = "./css_v8/courses_offered.css"
+    document.getElementById("root_css").href = "./css_v8/root.css"
+    document.getElementById("admissions_css").href = "./css_v8/admissions.css"
+
+    console.log(document.styleSheets)
+}
